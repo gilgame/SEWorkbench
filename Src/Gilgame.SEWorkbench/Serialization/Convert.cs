67f208b30a424f0b88186631fc1be7c5fc880e99
Gilgame <tdfellows@gmail.com>
2015-11-12 18:30:21 -0700

    Added serialization.

---
+++ Src/Gilgame.SEWorkbench/Serialization/Convert.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Gilgame.SEWorkbench.Serialization
+{
+    public static class Convert
+    {
+        public static string ToSerialized(object o)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(o.GetType());
+                XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces(
+                    new[] { XmlQualifiedName.Empty }
+                );
+
+                XmlWriterSettings settings = new XmlWriterSettings()
+                {
+                    OmitXmlDeclaration = true
+                };
+
+                StringBuilder result = new StringBuilder();
+                using (var writer = XmlWriter.Create(result, settings))
+                {
+                    serializer.Serialize(writer, o, namespaces);
+                }
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                // log error
+
+                return String.Empty;
+            }
+        }
+
+        public static object ToObject(string serialized)
+        {
+            try
+            {
+                Type type = GetType(serialized);
+                XmlSerializer serializer = new XmlSerializer(type);
+                using (var reader = new StringReader(serialized))
+                {
+                    return serializer.Deserialize(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                //log error
+
+                return new Object();
+            }
+        }
+
+        private static Type GetType(string serialized)
+        {
+            XDocument document = XDocument.Parse(serialized);
+
+            string root = document.Root.Name.ToString();
+            switch (root)
+            {
+                case "ProjectFile": return typeof(ProjectFile);
+
+                default: return typeof(Object);
+            }
+        }
+    }
+}
---
+++ Src/Gilgame.SEWorkbench/Serialization/ProjectFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Gilgame.SEWorkbench.Serialization
+{
+    [Serializable, XmlRoot("ProjectFile")]
+    public class ProjectFile
+    {
+        public string ProjectName { get; set; }
+    }
+}
