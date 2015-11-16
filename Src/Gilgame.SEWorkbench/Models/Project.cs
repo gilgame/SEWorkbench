3bd45c3de07b4203a7229a6bc3d5a40ad3365f75
Tim <tdfellows@gmail.com>
2015-11-16 07:16:52 -0700

    Added?

---
+++ Src/Gilgame.SEWorkbench/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace Gilgame.SEWorkbench.Models
+{
+    [Serializable]
+    [XmlRoot("Project")]
+    public class Project
+    {
+        // file missing?
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Path")]
+        public string Path { get; set; }
+
+        [XmlElement("RootItem")]
+        public ProjectItem RootItem { get; set; }
+    }
+}
