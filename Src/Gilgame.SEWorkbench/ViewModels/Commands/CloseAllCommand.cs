03ff4a596783ad416eb7709b93923988c09018bd
Tim <tdfellows@gmail.com>
2015-11-25 06:18:53 -0700

    Fixed file missing.

---
+++ Src/Gilgame.SEWorkbench/ViewModels/Commands/CloseAllCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Gilgame.SEWorkbench.ViewModels.Commands
+{
+    public class CloseAllCommand : ICommand
+    {
+        private readonly ProjectManagerViewModel _Manager;
+
+        public CloseAllCommand(ProjectManagerViewModel project)
+        {
+            _Manager = project;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_Manager != null)
+            {
+                _Manager.PerformCloseAll();
+            }
+        }
+    }
+}
