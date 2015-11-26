f3edb10051f8f777a557cded52981ca66a981822
Gilgame <tdfellows@gmail.com>
2015-11-26 13:46:04 -0700

    Added ability to copy over SE files necessary for execution.

---
+++ Src/Gilgame.SEWorkbench/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+using Gilgame.SEWorkbench.Services;
+
+namespace Gilgame.SEWorkbench
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (!SandboxCopied())
+            {
+                CopySandbox();
+            }
+
+            StartApp();
+        }
+
+        private static void StartApp()
+        {
+            Gilgame.SEWorkbench.App app = new Gilgame.SEWorkbench.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static bool SandboxCopied()
+        {
+            return File.Exists("Sandbox.Common.dll");
+        }
+
+        private static void CopySandbox()
+        {
+            string saveto = Directory.GetCurrentDirectory();
+
+            string sepath = Services.Registry.GetValue(Microsoft.Win32.RegistryHive.LocalMachine, Registry.K_SEROOT, Registry.V_SELOC).ToString();
+            if (String.IsNullOrEmpty(sepath))
+            {
+                sepath = GetSEPath();
+                if (String.IsNullOrEmpty(sepath))
+                {
+                    return;
+                }
+            }
+            else
+            {
+                sepath = Path.Combine(sepath, "Bin");
+            }
+
+            try
+            {
+                File.Copy(Path.Combine(sepath, "HavokWrapper.dll"), Path.Combine(saveto, "HavokWrapper.dll"));
+                File.Copy(Path.Combine(sepath, "InfinarioSDK.dll"), Path.Combine(saveto, "InfinarioSDK.dll"));
+                File.Copy(Path.Combine(sepath, "Sandbox.Common.dll"), Path.Combine(saveto, "Sandbox.Common.dll"));
+                File.Copy(Path.Combine(sepath, "Sandbox.Game.dll"), Path.Combine(saveto, "Sandbox.Game.dll"));
+                File.Copy(Path.Combine(sepath, "Sandbox.Graphics.dll"), Path.Combine(saveto, "Sandbox.Graphics.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.Direct2D1.dll"), Path.Combine(saveto, "SharpDX.Direct2D1.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.Direct3D11.dll"), Path.Combine(saveto, "SharpDX.Direct3D11.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.DirectInput.dll"), Path.Combine(saveto, "SharpDX.DirectInput.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.dll"), Path.Combine(saveto, "SharpDX.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.DXGI.dll"), Path.Combine(saveto, "SharpDX.DXGI.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.Toolkit.dll"), Path.Combine(saveto, "SharpDX.Toolkit.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.Toolkit.Graphics.dll"), Path.Combine(saveto, "SharpDX.Toolkit.Graphics.dll"));
+                File.Copy(Path.Combine(sepath, "SharpDX.XAudio2.dll"), Path.Combine(saveto, "SharpDX.XAudio2.dll"));
+                File.Copy(Path.Combine(sepath, "SteamSDK.dll"), Path.Combine(saveto, "SteamSDK.dll"));
+                File.Copy(Path.Combine(sepath, "System.Data.SQLite.dll"), Path.Combine(saveto, "System.Data.SQLite.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Audio.dll"), Path.Combine(saveto, "VRage.Audio.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.dll"), Path.Combine(saveto, "VRage.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Game.dll"), Path.Combine(saveto, "VRage.Game.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Game.XmlSerializers.dll"), Path.Combine(saveto, "VRage.Game.XmlSerializers.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Input.dll"), Path.Combine(saveto, "VRage.Input.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Library.dll"), Path.Combine(saveto, "VRage.Library.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Math.dll"), Path.Combine(saveto, "VRage.Math.dll"));
+                File.Copy(Path.Combine(sepath, "VRage.Native.dll"), Path.Combine(saveto, "VRage.Native.dll"));
+            }
+            catch (Exception ex)
+            {
+                // TODO log error
+                MessageBox.ShowError(ex.Message);
+                return;
+            }
+        }
+
+        private static string GetSEPath()
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                DefaultExt = ".exe",
+                Filter = "Space Engineers Executable (SpaceEngineers.exe)|SpaceEngineers.exe",
+                InitialDirectory = @"C:\Program Files (x86)\Steam\SteamApps\common\SpaceEngineers\Bin",
+            };
+
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result != null && result.Value == true)
+            {
+                string filename = dialog.FileName;
+                if (!String.IsNullOrEmpty(filename))
+                {
+                    return Path.GetDirectoryName(filename);
+                }
+            }
+            
+            return null;
+        }
+    }
+}
---
+++ Src/Gilgame.SEWorkbench/Services/Registry.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace Gilgame.SEWorkbench.Services
+{
+    public class Registry
+    {
+        public const string K_ROOT = @"SOFTWARE\Gilgame\SEWorkbench";
+        public const string K_SEROOT = @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244850";
+        public const string V_SELOC = "InstallLocation";
+
+        public static object GetValue(RegistryHive hive, string path, string name, object value = null, bool try64 = false)
+        {
+            object result = value;
+
+            RegistryKey key = GetBaseKey(hive, path, try64);
+            if (key != null)
+            {
+                result = key.GetValue(name, value);
+                if (result == null && !try64)
+                {
+                    result = GetValue(hive, path, name, value, true);
+                }
+            }
+            return result;
+        }
+
+        public static void SetValue(RegistryHive hive, string path, string name, object value = null)
+        {
+            RegistryKey key = GetSubKey(hive);
+
+            key = key.CreateSubKey(path);
+            if (key != null)
+            {
+                key.SetValue(name, value);
+            }
+        }
+
+        private static RegistryKey GetBaseKey(RegistryHive hive, string path, bool try64)
+        {
+            RegistryKey key = null;
+            if (try64)
+            {
+                key = RegistryKey.OpenBaseKey(hive, RegistryView.Registry64);
+                return key.OpenSubKey(path, false);
+            }
+            else
+            {
+                if (hive == RegistryHive.LocalMachine)
+                {
+                    return Microsoft.Win32.Registry.LocalMachine.OpenSubKey(path);
+                }
+                else
+                {
+                    return Microsoft.Win32.Registry.CurrentUser.OpenSubKey(path);
+                }
+            }
+        }
+
+        private static RegistryKey GetSubKey(RegistryHive hive)
+        {
+            RegistryKey key = null;
+            if (hive == RegistryHive.LocalMachine)
+            {
+                key = Microsoft.Win32.Registry.LocalMachine;
+            }
+            else
+            {
+                key = Microsoft.Win32.Registry.CurrentUser;
+            }
+            return key;
+        }
+    }
+}
